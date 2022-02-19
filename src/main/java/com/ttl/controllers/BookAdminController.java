cbe07d506aa5aa3559a6eadc6089349cc17556d3
Aristokrat <olrpgaristokrat@gmail.com>
2022-02-19 23:24:21 +0200

    Lab 3

--- src/main/java/com/ttl/controllers/BookAdminController.java
+++ src/main/java/com/ttl/controllers/BookAdminController.java
@@ -1,19 +1,16 @@
 package com.ttl.controllers;
 
 import com.ttl.dto.BookDTO;
+import com.ttl.entities.Book;
 import com.ttl.services.BookService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 @RequestMapping("/bookAdmin")
 public class BookAdminController {
     private final BookService bookService;
@@ -25,23 +22,50 @@ public class BookAdminController {
 
     @Operation(summary = "Create book", description = "Create new book with entered values")
     @PostMapping("/book")
-    public Integer postBook(@RequestBody BookDTO book) {
-        return bookService.create(book);
+    public String postBook(@ModelAttribute(value="book") BookDTO book) {
+        bookService.create(book);
+        return "index";
     }
 
     @Operation(summary = "Update book", description = "Update book with entered values")
-    @PutMapping("/book/{id}")
-    public void putBook(@RequestBody BookDTO book,
+    @PostMapping("/book/{id}")
+    public String putBook(@ModelAttribute BookDTO book,
         @Parameter(description = "Book id")
         @PathVariable Integer id){
         book.setId(id);
         bookService.update(book);
+        return "index";
     }
 
     @Operation(summary = "Delete book", description = "Delete book by entered id")
-    @DeleteMapping("/book/{id}")
-    public void deleteBook(@Parameter(description = "Book id")
+    @PostMapping("/deleteBook/{id}")
+    public String deleteBook(@Parameter(description = "Book id")
         @PathVariable Integer id){
         bookService.delete(id);
+        return "index";
+    }
+
+    @GetMapping("/addBook")
+    public ModelAndView addBook() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("book", new BookDTO());
+        modelAndView.setViewName("addBook");
+        return modelAndView;
+    }
+
+    @GetMapping("/updateBook")
+    public ModelAndView updateBook() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("book", new BookDTO());
+        modelAndView.setViewName("updateBook");
+        return modelAndView;
+    }
+
+    @GetMapping("/deleteBook")
+    public ModelAndView deleteBook() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("book", new BookDTO());
+        modelAndView.setViewName("deleteBook");
+        return modelAndView;
     }
 }
--- src/main/java/com/ttl/controllers/BookSearchController.java
+++ src/main/java/com/ttl/controllers/BookSearchController.java
@@ -7,17 +7,16 @@ import java.util.List;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
-@RestController
+@Controller
 @RequestMapping("/bookSearch")
 public class BookSearchController {
     private final BookService bookService;
@@ -29,27 +28,39 @@ public class BookSearchController {
 
     @Operation(summary = "Get books by author name", description = "Get books which author name contains entered author name")
     @GetMapping("/getByAuthorName/{authorName}")
-    public List<FullBookDTO> searchByAuthorName(@Parameter(description = "Author name by which books will be searched")
-        @PathVariable String authorName){
+    public RedirectView searchByAuthorName(@Parameter(description = "Author name by which books will be searched")
+        @PathVariable String authorName,
+        RedirectAttributes redir){
         List<FullBookDTO> books = bookService.getAllBooksByAuthorName(authorName);
 
-        return books;
+        var redirectView= new RedirectView("/",true);
+        redir.addFlashAttribute("books", books);
+
+        return redirectView;
     }
     @Operation(summary = "Get books by title", description = "Get books which title contains entered title")
     @GetMapping("/getByTitle/{title}")
-    public List<FullBookDTO> searchByTitle(@Parameter(description = "Book title by which books will be searched")
-        @PathVariable String title){
+    public RedirectView searchByTitle(@Parameter(description = "Book title by which books will be searched")
+        @PathVariable String title,
+        RedirectAttributes redir){
         List<FullBookDTO> books = bookService.getAllBooksByTitle(title);
 
-        return books;
+        var redirectView= new RedirectView("/",true);
+        redir.addFlashAttribute("books", books);
+
+        return redirectView;
     }
 
     @Operation(summary = "Get books by keywords", description = "Get books which description contains entered keywords")
     @GetMapping("/getByKeyWords/{keywords}")
-    public List<FullBookDTO> searchByKeyWords(@Parameter(description = "Keywords by which books will be searched")
-        @PathVariable String keywords){
+    public RedirectView searchByKeyWords(@Parameter(description = "Keywords by which books will be searched")
+        @PathVariable String keywords,
+        RedirectAttributes redir){
         List<FullBookDTO> books = bookService.getAllBooksByKeyWords(keywords);
 
-        return books;
+        var redirectView= new RedirectView("/",true);
+        redir.addFlashAttribute("books", books);
+
+        return redirectView;
     }
 }
